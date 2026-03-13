8499fe25d0af6456f991709732acaa40c77fbbda
topcue <topcue1019@gmail.com>
2026-03-13 13:52:34 +0900

    Fix config.py

--- code/config.py
+++ code/config.py
@@ -1,10 +1,14 @@
-PROJECT_PATH = 'C:\\Users\\gary\\Desktop\\RansomRadar'
+PROJECT_PATH = r'C:\Users\user\workspace\RansomRadar'
 
-RAW_ROOT_PATH = f'{PROJECT_PATH}\\dataset\\raw'
-HPC_ROOT_path = f'{PROJECT_PATH}\\dataset\\hpcdata'
-IRP_ROOT_PATH = f'{PROJECT_PATH}\\dataset\\irplog'
-PARSER_PATH = f'{PROJECT_PATH}\\helper\\PARSER\\PARSER\\bin\\Release\\PARSER.exe'
-PARSER_PROFILE_PATH = f'{PROJECT_PATH}\\helper\\parse.wpaProfile'
-FEATURE_PATH = f'{PROJECT_PATH}\\features'
-RESULT_PATH = f'{PROJECT_PATH}\\result'
-MODEL_PATH = f'{PROJECT_PATH}\\models'
+RAW_ROOT_PATH = rf'{PROJECT_PATH}\dataset\raw'
+HPC_ROOT_path = rf'{PROJECT_PATH}\dataset\hpcdata'
+IRP_ROOT_PATH = rf'{PROJECT_PATH}\dataset\irplog'
+
+PARSER_PATH = rf'{PROJECT_PATH}\helper\PARSER\PARSER\bin\Release\PARSER.exe'
+PARSER_PROFILE_PATH = rf'{PROJECT_PATH}\helper\parse.wpaProfile'
+
+FEATURE_PATH = rf'{PROJECT_PATH}\features'
+RESULT_PATH = rf'{PROJECT_PATH}\result'
+MODEL_PATH = rf'{PROJECT_PATH}\models'
+
+# EOF
